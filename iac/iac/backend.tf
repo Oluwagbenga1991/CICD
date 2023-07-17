d9d3c377a6d551e1203dfee0fdcb0ef7fea5f20d
Oluwagebenga Harrison <holuwagebenga@aol.com>
2023-07-16 21:19:06 -0400

    updated cicd terra code

--- iac/iac/backend.tf
+++ iac/iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = 
-    key            = 
-    region         = 
-    dynamodb_table = 
+    bucket         = "terra-rem-state-07"
+    key            = "rentzone-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "Terraform-state-lock"
   }
 }
